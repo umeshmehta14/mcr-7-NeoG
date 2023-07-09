f57b06d80528e409ea7cab3e5eb9a1f2868af185
umeshmehta14 <ishaanmehta782@gmail.com>
2023-07-09 22:00:12 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
 import Country from "./Pages/Country/Country";
 import City from "./Pages/City/City";
 import SinglePage from "./Pages/Single page/SinglePage";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
     <div className="App">
+      {location?.pathname === "/" ? (
+        ""
+      ) : (
+        <AiOutlineArrowLeft className="back-btn" onClick={() => navigate(-1)} />
+      )}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="country/:continentName" element={<Country />} />
